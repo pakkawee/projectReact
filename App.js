42db214d6c09e96c3435a7368e352e3c147d3a6c
pakkawee <pakkawee.1@gmail.com>
2022-07-20 11:25:44 +0700

    commit

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+//import ButtonExample from './components/ButtonExample'
 // import Logo from './components/Logo'
 // import LotsOfGreetings from './components/LotsOfGreetings'
 // import MyCustomTextWith from './components/MyCustomTextWith'
 // import Count from './components/Count'
 // import InputText from './components/InputText'
-import UserNameandPassword from './components/UserNameandPassword'
-
+//import UserNameandPassword from './components/UserNameandPassword'
+//import AlertComponent from './components/AlertComponent'
+//import ImageWithTextInput from './components/ImageWithTextInput'
+import Touchable_Example from './components/Touchable_Example'
 const App = () => {
   return (
-    <View >
+    <View style = {{flex : 1}}>
       {/* <Text>App</Text> */}
       {/* <Logo/> */}
       {/* <LotsOfGreetings/> */}
       {/* <MyCustomTextWith/> */}
       {/* <Count/> */}
       {/* <InputText/> */}
-      <UserNameandPassword/>
+      {/* {<UserNameandPassword/>} */}
+      {<Touchable_Example/>}
     </View>
   )
 }
